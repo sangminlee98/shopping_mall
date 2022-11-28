56458d5632d31d3634200ec0a9dba54f1518398d
sangminlee98 <tkdals0975@naver.com>
2022-11-29 03:34:01 +0900

    feat : gnb 생성

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes";
 import { getClient } from "./queryClient";
+import Gnb from "./components/gnb";
 
 export default function App() {
   const elem = useRoutes(routes);
   const queryClient = getClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <Gnb />
       {elem}
       <ReactQueryDevtools />
     </QueryClientProvider>
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import GlobalLayout from './pages/_layout'
 
 const DynamicIndex = React.lazy(() => import('./pages/index'));
+const DynamicCartIndex = React.lazy(() => import('./pages/cart/index'));
 const DynamicProductsIndex = React.lazy(() => import('./pages/products/index'));
 const DynamicProductsId = React.lazy(() => import('./pages/products/[id]'));
 
@@ -13,6 +14,7 @@ export const routes = [
     element: <GlobalLayout />,
     children: [
       { path: '/', element: <DynamicIndex />, index: true},
+      { path: '/cart', element: <DynamicCartIndex />, index: true},
       { path: '/products', element: <DynamicProductsIndex />, index: true},
       { path: '/products/:id', element: <DynamicProductsId />, },
     ]
@@ -21,6 +23,7 @@ export const routes = [
 
 export const pages = [
   { route: '/' },
+  { route: '/cart' },
   { route: '/products' },
   { route: '/products/:id' },
 ]
